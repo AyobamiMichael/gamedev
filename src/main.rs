3ad640f0db663d50a356273567f74485c6ab8405
Ayobami <ayobamiwealth@gmail.com>
2024-07-07 11:01:15 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ async fn main() {
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_circle(screen_width() /2.0 - 30.0, screen_height()/ 2.0 - 30.0, 15.0,  YELLOW);
 
-        draw_circle(screen_width() - 35.0, screen_height() - 35.0, 15.0, ORANGE);
+        draw_circle(screen_width()/ 2.0 - 50.0, screen_height()/ 2.0 - 60.0, 15.0, ORANGE);
 
 
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
